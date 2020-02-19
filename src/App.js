e702ae1353cdd81f495124245f120322e11d2976
bioinfo <lucas208@ufrn.edu.br>
2020-02-19 16:21:51 -0300

    até a aula 29

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react';
 import { CardList } from './components/card-list/card.list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 import './App.css';
 
@@ -8,8 +9,10 @@ class App extends Component {
     super();
   
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
+
   }
 
   componentDidMount(){
@@ -18,11 +21,27 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
+  }
+
 
   render(){
+    //const monsters = this.state.monsters;
+    //const searchField = this.state.searchField;
+    //equivale a:
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+    
     return (
-      <div className="App">
-        <CardList monsters = {this.state.monsters}/>
+      <div className='App'>
+        <SearchBox 
+        placeholder= 'search monsters'
+        handleChange = {this.handleChange}
+        />
+        <CardList monsters = {filteredMonsters}/>
       </div>
     );
   }
