77da2b6896d20fad18f68252cf3686c9cf9b6f37
Lucas <lucas208@ufrn.edu.br>
2020-02-20 14:09:02 -0300

    Updates

---
+++ precache-manifest.1dbbb7cd46c25051f04b700724f1ec91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6750d8186958c42003990e4c21263e40",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "11fa4b2a641d79b017b8",
+    "url": "/monsters/static/css/main.5a487302.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fa4b2a641d79b017b8",
+    "url": "/monsters/static/js/main.7a50a4e0.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
